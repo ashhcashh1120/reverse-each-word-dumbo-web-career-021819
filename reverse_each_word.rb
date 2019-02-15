e5528c78c5f88a19c1da3abfb53fb81f1c044cba
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 00:51:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   new_string = string.split(" ")
-  reversed_string = new_string.each {|x| x.reverse}
+  reversed_string = new_string.each {|x| x.reverse!}
   return reversed_string.join(" ")
 end
